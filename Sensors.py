82141ba8ede961e35dd9bf4ea5c85ee58470be9d
Jarosław Kozak <jaroslaw.kozak@hotmail.com>
2018-12-26 23:02:41 +0100

    fix and updated to sensor service

--- Sensors.py
+++ Sensors.py
@@ -1,4 +1,24 @@
+import Adafruit_DHT
+import time
+
+
 class DHT11:
-    def __init__(self, temp, humid):
-        self.temperature = temp
-        self.humidity = humid
+    def __init__(self, gpio):
+        # Set sensor type : Options are DHT11,DHT22 or AM2302
+        self.sensor = Adafruit_DHT.DHT11
+        self.gpio = gpio
+        self.humidity = None
+        self.temperature = None
+
+    def read(self):
+        # Use read_retry method. This will retry up to 15 times to
+        # get a sensor reading (waiting 2 seconds between each retry).
+        self.humidity, self.temperature = Adafruit_DHT.read_retry(self.sensor, self.gpio)
+        return self
+
+    def data(self):
+        return {
+            'timestamp': str(int(round(time.time(), 0))),
+            'temperature': self.temperature,
+            'humidity': self.humidity
+        }
--- SensorsApiService.py
+++ SensorsApiService.py
@@ -1,45 +1,31 @@
-from flask import request, Response
+from flask import Response
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
 from Sensors import DHT11
-import Adafruit_DHT
 import json
+import time
 
 app = Flask('sensors-api')
-
+dht11 = DHT11(17)
 
 def write_sensor_to_file():
-    dht = read_sensor()
-    with open('sensor-data.txt', 'ab+') as f:
-        print('added: Temperature: ' + dht.temperature + ', Humidity: ' + dht.humidity)
-        f.write('Temperature: ' + dht.temperature + ', Humidity: ' + dht.humidity + '\n')
+    dht11.read()
+    with open('sensor-data.txt', 'a+') as f:
+        f.write('timestamp: ' + str(int(round(time.time(), 0)))
+                + 'temperature: '
+                + str(dht11.temperature)
+                + ', humidity: '
+                + str(dht11.humidity) + '\n')
 
 
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(write_sensor_to_file, 'interval', minutes=1)
+sched.add_job(write_sensor_to_file, 'interval', seconds=5)
 sched.start()
-
-
-def read_sensor():
-    # Set sensor type : Options are DHT11,DHT22 or AM2302
-    sensor = Adafruit_DHT.DHT11
-
-    # Set GPIO sensor is connected to
-    gpio = 17
-
-    # Use read_retry method. This will retry up to 15 times to
-    # get a sensor reading (waiting 2 seconds between each retry).
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
-
-    dht = DHT11(temperature, humidity)
-    return dht
-
 
 @app.route("/metrics", methods=['GET'])
 def get_sensors_data():
-    dht = read_sensor()
-
-    json_message = json.dumps(dht.__dict__)
+    dht11.read()
+    json_message = json.dumps(dht11.data())
     return Response(json_message, status=200, mimetype='application/json')
 
 
