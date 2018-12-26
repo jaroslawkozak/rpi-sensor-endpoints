6e943f55b422107f45c5c1908bb0bb10dec4cd27
Jarosław Kozak <jaroslaw.kozak@hotmail.com>
2018-12-26 14:11:14 +0100

    added scheduled metric saving to file

--- SensorsApiService.py
+++ SensorsApiService.py
@@ -1,15 +1,26 @@
 from flask import request, Response
 from flask import Flask
-import Adafruit_DHT
+from apscheduler.schedulers.background import BackgroundScheduler
 from Sensors import DHT11
+import Adafruit_DHT
 import json
 
 app = Flask('sensors-api')
 
-@app.route("/metrics", methods=['GET'])
-def get_sensors_data():
+
+def write_sensor_to_file():
+    dht = read_sensor()
+    with open('sensor-data.txt', 'ab+') as f:
+        print('added: Temperature: ' + dht.temperature + ', Humidity: ' + dht.humidity)
+        f.write('Temperature: ' + dht.temperature + ', Humidity: ' + dht.humidity + '\n')
 
 
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(write_sensor_to_file, 'interval', minutes=1)
+sched.start()
+
+
+def read_sensor():
     # Set sensor type : Options are DHT11,DHT22 or AM2302
     sensor = Adafruit_DHT.DHT11
 
@@ -21,6 +32,12 @@ def get_sensors_data():
     humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
 
     dht = DHT11(temperature, humidity)
+    return dht
+
+
+@app.route("/metrics", methods=['GET'])
+def get_sensors_data():
+    dht = read_sensor()
 
     json_message = json.dumps(dht.__dict__)
     return Response(json_message, status=200, mimetype='application/json')
