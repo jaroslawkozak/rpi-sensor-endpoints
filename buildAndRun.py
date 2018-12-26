f56f0ec4c3b5ca54d6c56139bfd9f52a598218cc
Jarosław Kozak <jaroslaw.kozak@hotmail.com>
2018-12-26 23:29:08 +0100

    updates to build script

--- buildAndRun.py
+++ buildAndRun.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import os
-os.system('sudo docker build . -f Dockerfile-rpi')
+os.system('sudo docker stop sensor-service')
+os.system('sudo docker rm sensor-service')
+os.system('sudo docker image rm sensor-service')
+os.system('sudo docker build --no-cache . -f Dockerfile-rpi -t sensor-service:latest')
 os.system('sudo docker run --privileged --name sensor-service -d -p 5555:5555 sensor-service')
-
 
 
